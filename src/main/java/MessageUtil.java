ebd30d2e5f9d0d3d5e72d909bc0bcb849371adea
Bodero <bodossie@3c06300a6e33.ant.amazon.com>
2021-12-03 15:09:18 -0800

    Hello

--- src/main/java/MessageUtil.java
+++ src/main/java/MessageUtil.java
@@ -11,7 +11,7 @@ public class MessageUtil {
     }
   
     public String salutationMessage() {
-      message = "Hola!" + message;
+      message = "Hi!" + message;
       System.out.println(message);
       return message;
     }
